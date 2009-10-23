2a786710b493589d6f9cebc4208e33dd8524f6c4
Lake Denman <lake@lake.local>
2009-10-22 21:58:43 -0400

    Revert "Add new style"
    This reverts commit ba07539fd49705c3860f0da1a9693b1fcbed56f9.

--- css/images/nikpawlak.com/v1/js/analytics.js
+++
@@ -1,8 +0,0 @@
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write("\<script src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'>\<\/script>" );
-
-
-var pageTracker = _gat._getTracker("UA-2022371-4");
-pageTracker._initData();
-pageTracker._trackPageview();
-
