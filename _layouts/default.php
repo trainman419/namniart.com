fd23404358f7a16d1ed4c7bdb2749a342376d105
Austin <legotown@aol.com>
2010-12-12 00:39:08 -0800

    Adding dates to layouts and data format to config.

---
+++ _layouts/default.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta content="text/html; charset=utf-8" http-equiv="content-type" />
+  <title>namniart</title>
+  <link rel="stylesheet" href="/css/syntax.css" type="text/css" />
+  <!-- Homepage CSS -->
+  <link rel="stylesheet" href="/css/style-nik.css" type="text/css" media="screen, projection" />
+</head>
+
+<body id="post">
+  <div id='content-wrapper'>
+    <h1 id='page-header'>
+      <a href="/">
+        namniart
+      </a>
+    </h1>
+    <div id='header'>
+      <a href="/">Home</a> | 
+      <a href="/about.html">About</a> |
+      <a href="/projects.html">Projects</a>
+    </div>
+
+    <div id='content'>
+      <h2>{{ page.title }}</h2>
+      {{ content }}
+    </div>
+
+    <div id= 'footer'>
+      <a href="http://github.com/trainman419">Github</a>
+    </div>
+   </div>
+  </body>
+</html>
