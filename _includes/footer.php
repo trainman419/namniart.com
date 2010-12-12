2b8e1e933d977ba2bedef8ba9a096d0df3249dae
Austin <legotown@aol.com>
2010-12-12 01:25:44 -0800

    Adding style credit to footer.

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,5 +1,6 @@
 
     <div id= 'footer'>
+      <a href+"http://lakedenman.com/">Style Inspired by Lake Denman</a> | 
       <a href="http://github.com/trainman419">Github</a>
     </div>
     </div>
