c5c937016754538d367169e945a21c0eea982ed7
Austin <legotown@aol.com>
2010-12-12 00:50:06 -0800

    Adding date to post title in index.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ layout: default
 <ul class="posts">
   {% for post in site.posts %}
     <li>
-        <h2 class="title"><a href="{{ post.url }}">{{ post.title }}</a></h2>
+        <h2 class="title"><a href="{{ post.url }}">{{ post.date | date: site.datefmt }} - {{ post.title }}</a></h2>
           <p>
             {{ post.excerpt }}
           </p>
