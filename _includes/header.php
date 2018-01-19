17d8e57dadfb72cd3dd3fa1b4b45e711859690b6
Austin <namniart+github@gmail.com>
2018-01-18 22:22:15 -1000

    Add google analytics

--- _includes/header.php
+++ _includes/header.php
@@ -3,6 +3,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112740753-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-112740753-1');
+  </script>
   <meta content="text/html; charset=utf-8" http-equiv="content-type" />
   <title>namniart{{ extratitle }}</title>
   <link rel="stylesheet" href="/css/syntax.css" type="text/css" />
