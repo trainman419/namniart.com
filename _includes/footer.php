79428dc916c2843a11c2dda91a918781e97b67f0
trainman419 <legotown@aol.com>
2023-07-05 21:37:36 +0000

    Remove twitter from footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -2,7 +2,6 @@
     <div id= 'footer'>
       <a href="http://lakedenman.com/">Style Inspired by Lake Denman</a> | 
       <a href="http://github.com/trainman419">Github</a> |
-      <a href="http://twitter.com/namniart">Twitter</a>
     </div>
     </div>
   </body>
